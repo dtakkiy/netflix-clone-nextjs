c5bbaa000c76b7d239709354d0ed72e363c8abd4
dtakkiy <dtakkiy00@gmail.com>
2022-12-12 17:54:36 +0900

    🎉: first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Button } from '@/components/Button/Button';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Button />
+    </>
+  );
+};
+
+export default Home;
