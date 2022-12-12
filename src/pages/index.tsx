8a10435b54f9116fecbfb6bee0b8d9e00c746118
dtakkiy <dtakkiy00@gmail.com>
2022-12-13 07:34:57 +0900

    🐛: fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
       <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
       <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
       <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
-      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />{' '}
+      <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />{' '}
     </>
   );
 };
